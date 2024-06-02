807b1ec5453accee6b86dcd9bac8a4bb0269ec8c
fajar <rokhman.fajar@gmail.com>
2024-06-02 17:43:02 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 "use strict";
 let maxBack = 8;
-let max = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-let ratio = (document.body.scrollTop || document.documentElement.scrollTop) / max;
+let wrapper = document.querySelector("div.hal-wrapper");
+let max = wrapper.scrollHeight;
+let ratio = (wrapper.scrollTop) / max;
 let ratioN = Math.floor(ratio * (maxBack));
 let maxBack2 = 7;
 let backEl = document.body.querySelector("div#back").children;
@@ -9,6 +10,7 @@ let backElJml = backEl.length;
 let backAr = [];
 let ctr = 0;
 function update() {
+    updateRatio();
     backAr.forEach((item, idx) => {
         if (idx != ratioN) {
             let o = parseFloat(item.style.opacity);
@@ -34,15 +36,20 @@ function update() {
         }
     });
     // ctr++;
+    // console.log(wrapper.scrollTop);
     if (ctr < 10) {
         window.requestAnimationFrame(update);
     }
 }
+function debug(str) {
+    document.body.querySelector("div.debug").innerHTML = str;
+}
 function updateRatio() {
-    ratio = (document.body.scrollTop || document.documentElement.scrollTop) / max;
+    ratio = (wrapper.scrollTop) / max;
     ratioN = Math.floor(ratio * (maxBack));
     if (ratioN > maxBack2)
         ratioN = maxBack2;
+    debug("rationN " + ratioN + "/ratio " + ratio + "/max " + max);
 }
 updateRatio();
 for (let i = 0; i < backElJml; i++) {
@@ -67,5 +74,6 @@ for (let i = 0; i < backElJml; i++) {
 window.onscroll = () => {
     updateRatio();
     console.log(ratioN);
+    console.log(wrapper.scrollTop);
 };
 window.requestAnimationFrame(update);
